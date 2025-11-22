1bcf5392f5c012a40b979d33abf3e657e8c4013f
GongMyung-Cho <besoullight@gmail.com>
2025-11-22 23:34:13 +0900

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://omchtafaqgkdwcrwscrp.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tY2h0YWZhcWdrZHdjcndzY3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODIyNjMsImV4cCI6MjA3NDQ1ODI2M30.vGV6Gfgi1V8agiwL03ho2R7BAwv4CrTp6-RGH0S3-4g";
