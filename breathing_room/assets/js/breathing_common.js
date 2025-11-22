ffe23cff749d6072dc42ad750d191a80822a08b7
GongMyung-Cho <besoullight@gmail.com>
2025-11-22 23:37:01 +0900

    Create breathing_common.js

---
+++ breathing_room/assets/js/breathing_common.js
@@ -0,0 +1,10 @@
+// 페이드인, 스크롤 애니메이션 효과
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".card").forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
