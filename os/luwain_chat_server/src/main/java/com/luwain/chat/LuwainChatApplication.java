d1d3e08a271961489c31348f38ad0f300ace8e9d
GongMyung-Cho <besoullight@gmail.com>
2025-11-29 08:57:05 +0900

    Create LuwainChatApplication.java

---
+++ os/luwain_chat_server/src/main/java/com/luwain/chat/LuwainChatApplication.java
@@ -0,0 +1,11 @@
+package com.luwain.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuwainChatApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(LuwainChatApplication.class, args);
+  }
+}
