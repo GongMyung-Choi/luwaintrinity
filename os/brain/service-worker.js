d24e86dd77b104156dd148f5f59431a16e36ccc0
GongMyung-Cho <besoullight@gmail.com>
2025-12-07 13:52:22 +0900

    Update service-worker.js

--- os/brain/service-worker.js
+++ os/brain/service-worker.js
@@ -1,10 +1,3 @@
-self.addEventListener("install", () => {
-    console.log("BRAIN 설치됨");
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-    console.log("BRAIN 활성화됨");
-});
-
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => {});
 self.addEventListener("fetch", () => {});
